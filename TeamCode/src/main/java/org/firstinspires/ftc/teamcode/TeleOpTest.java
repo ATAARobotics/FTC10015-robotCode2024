92fa2a8b12dee58ecd59832d30758907ce99ca56
witchdocter <witchdocter@warrens.fun>
2023-10-03 13:06:15 -0600

    doing some practice outline stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Kiwi: TeleOp", group="Opmode")
+public class TeleOpTest extends OpMode   {
+
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+
+    @Override
+    public void loop() {
+}
