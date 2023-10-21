29f4bacfae5fb0b7d437f55a60aa53653898eb2e
meejah <meejah@meejah.ca>
2023-10-20 20:41:38 -0600

    add some DPAD controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import android.util.Size;
 
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -34,6 +36,8 @@ public class TeleOpTest extends OpMode   {
     public Motor motor_bl = null;
     public Motor motor_br = null;
 
+    public GamepadEx driver = null;
+
     MecanumDrive drivebase = null;
     IMU imu;
     PIDController headingControl = null;
@@ -54,6 +58,8 @@ public class TeleOpTest extends OpMode   {
 
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
+        driver = new GamepadEx(gamepad1);
+
         // Now initialize the IMU with this mounting orientation
         // Note: if you choose two conflicting directions, this initialization will cause a code exception.
         imu.initialize(new IMU.Parameters(orientationOnRobot));
@@ -63,11 +69,16 @@ public class TeleOpTest extends OpMode   {
         motor_bl = new Motor(hardwareMap, "BL_Drive");
         motor_br = new Motor(hardwareMap, "BR_Drive");
 
-        headingControl = new PIDController(0.2, 0.0, 0.0);
+        // in "turbe" mode, 0.2 + 0.1 + 0.0 was oscilating a lot (but was good in non-turbo mode)
+        headingControl = new PIDController(0.08, 0.05, 0.0);
         // using ftc-lib for driving
         drivebase = new MecanumDrive(motor_fl, motor_fr, motor_bl, motor_br);
-        drivebase.setMaxSpeed(0.5);
+        drivebase.setMaxSpeed(0.6);
 
+        //init_vision();
+    }
+
+    public void init_vision() {
 
 
         // vision (from the example code)
@@ -122,21 +133,40 @@ public class TeleOpTest extends OpMode   {
 
     @Override
     public void loop() {
+        driver.readButtons();
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         double heading = orientation.getYaw(AngleUnit.DEGREES);
         //correction = target_heading - heading;
         correction = headingControl.calculate(heading);
+
+        if (driver.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
+            headingControl.setSetPoint(0.0);
+        } else if (driver.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) {
+            headingControl.setSetPoint(90.0);
+        } else if (driver.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) {
+            headingControl.setSetPoint(-90.0);
+        } else if (driver.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) {
+            headingControl.setSetPoint(0.0);
+        }
+
+        // triggers return 0.0 -> 1.0 "more than 0.5" is "more than half pressed"
+        if (driver.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.5) {
+            drivebase.setMaxSpeed(0.85);
+        } else {
+            drivebase.setMaxSpeed(0.55);
+        }
         // tell ftclib its inputs
         drivebase.driveFieldCentric(
-                gamepad1.right_stick_x,
-                -gamepad1.right_stick_y,
+                driver.getRightX(),
+                -driver.getRightY(),
+               /// driver.getLeftX(),
                 -correction , //gamepad1.left_stick_x,
                 heading
         );
         //imu stuff
         telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", heading);
-        telemetry.addData("correction","%2f Deg.(correction) %.2f", correction, (correction/180.0));
-        telemetryTfod();
+        telemetry.addData("target","%2f target heading", target_heading);
+        //telemetryTfod();
         telemetry.update();
     }
 
