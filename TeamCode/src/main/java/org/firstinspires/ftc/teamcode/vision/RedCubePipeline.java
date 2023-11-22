e1a8e2432e09c53f736d164ccf93e05917283c5f
witchdoctor <witchdoctor>
2023-11-22 02:46:34 -0700

    maybe colors work?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/RedCubePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/RedCubePipeline.java
@@ -16,51 +16,14 @@ public class RedCubePipeline extends OpenCvPipeline {
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2HSV);
-        Mat thresh = new Mat();
         Core.inRange(
                 input,
+                // red airplane paper
                 new Scalar(120, 120, 120),
                 new Scalar(160, 180, 160),
-                thresh
+                input
         );
 
-        Mat edges = new Mat();
-        Imgproc.Canny(thresh, edges, 100, 300);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        MatOfPoint2f[] contoursPoly  = new MatOfPoint2f[contours.size()];
-        Rect[] boundRect = new Rect[contours.size()];
-        for (int i = 0; i < contours.size(); i++) {
-            contoursPoly[i] = new MatOfPoint2f();
-            Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
-            boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
-        }
-
-        Imgproc.cvtColor(thresh, thresh, Imgproc.COLOR_GRAY2BGR);
-
-        double width = 640;
-        // Iterate and check whether the bounding boxes
-        // cover left and/or right side of the image
-        double left_x = 0.25 * width;
-        double right_x = 0.75 * width;
-        boolean left = false; // true if regular stone found on the left side
-        boolean right = false; // "" "" on the right side
-        for (int i = 0; i != boundRect.length; i++) {
-            if (boundRect[i].x < left_x)
-                left = true;
-            if (boundRect[i].x + boundRect[i].width > right_x)
-                right = true;
-
-            // draw red bounding rectangles on mat
-            // the mat has been converted to HSV so we need to use HSV as well
-            Imgproc.rectangle(thresh, boundRect[i], new Scalar(255, 76.9, 89.8));
-        }
-
-        // draw some debug
-        Imgproc.rectangle(thresh, new Point(10, 10), new Point(200, 200), new Scalar(255, 0, 0), 2);
-        return thresh;
+        return input;
     }
 }
