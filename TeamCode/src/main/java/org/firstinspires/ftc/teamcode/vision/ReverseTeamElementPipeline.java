94a7382cdeb8aa4be2ebe9c174084e268b3b94b1
meejah <meejah@meejah.ca>
2023-12-02 00:35:05 -0700

    experiment

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/ReverseTeamElementPipeline.java
@@ -0,0 +1,118 @@
+package org.firstinspires.ftc.teamcode.vision;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseTeamElementPipeline extends OpenCvPipeline {
+    public String result = "unknown";
+    Scalar min;
+    Scalar max;
+    public Mat processed;
+    public Mat annotated;
+    public ReverseTeamElementPipeline() {
+        min = new Scalar(100, 100, 100);
+        max = new Scalar(160, 255, 255);
+        processed = new Mat();
+        annotated = new Mat();
+    }
+    public ReverseTeamElementPipeline(boolean red) {
+        if (red) {
+            min = new Scalar(100, 100, 100);
+            max = new Scalar(160, 255, 255);
+        } else {
+            // blue
+            min = new Scalar(22, 31, 69);
+            max = new Scalar(69, 255, 255);
+        }
+        processed = new Mat();
+        annotated = new Mat();
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        // don't create new Mat's in this method? apparently ... Mat processed = new Mat();
+        Imgproc.cvtColor(input, processed, Imgproc.COLOR_BGR2HSV);
+
+        // filter for one colour
+        Core.inRange(processed, min, max, processed);
+
+        //Imgproc.dilate(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+        //Imgproc.dilate(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+        //Imgproc.dilate(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+
+        int width = 50;
+        int height = 55;
+
+        int x0 = 100;
+        int y0 = 236;
+        int x1 = 330;
+        int y1 = 214;
+        int x2 = 560;
+        int y2 = 247;
+
+        // count how many pixels are "on" in each region of interest
+
+        int left = 0;
+        for (int x=x0; x < x0 + width; x++) {
+            for (int y=y0; y < y0 + height; y++) {
+                double[] px = input.get(y, x);
+                if (px != null && (int)px[0] > 128) {
+                    left++;
+                }
+            }
+        }
+
+        int mid = 0;
+        for (int x=x1; x < x1 + width; x++) {
+            for (int y=y1; y < y1 + height; y++) {
+                double[] px = input.get(y, x);
+                if (px != null && (int)px[0] > 128){
+                    mid++;
+                }
+            }
+        }
+
+        int right = 0;
+        for (int x=x2; x < x2 + width; x++) {
+            for (int y=y2; y < y2 + height; y++) {
+                double[] px = input.get(y, x);
+                if (px != null && (int)px[0] > 128) {
+                    right++;
+                }
+            }
+        }
+
+
+        Scalar red = new Scalar(255, 0, 0);
+        Scalar green = new Scalar(0, 255, 0);
+        int biggest = Math.max(left, Math.max(mid, right));
+        if (biggest > 100) {
+            if (left == biggest) { result = "left";  }
+            if (mid == biggest) { result = "middle"; }
+            if (right == biggest) { result = "right"; }
+        }
+
+
+        input.copyTo(annotated);
+        Imgproc.putText(annotated, "L:" + left, new Point(x0, y0), Imgproc.FONT_HERSHEY_PLAIN, 1, red);
+        Imgproc.putText(annotated, "M:" + mid, new Point(x1, y1), Imgproc.FONT_HERSHEY_PLAIN, 1, red);
+        Imgproc.putText(annotated, "R:" + right, new Point(x2, y2), Imgproc.FONT_HERSHEY_PLAIN, 1, red);
+        Imgproc.putText(annotated, result, new Point(0, 100), Imgproc.FONT_HERSHEY_PLAIN, 1, green);
+
+        Imgproc.rectangle(annotated, new Point(x0, y0), new Point(x0 + width, y0 + width), result.equals("left") ? green : red, 2);
+        Imgproc.rectangle(annotated, new Point(x1, y1), new Point(x1 + width, y1 + width), result.equals("middle") ? green : red, 2);
+        Imgproc.rectangle(annotated, new Point(x2, y2), new Point(x2 + width, y2 + width), result.equals("right") ? green : red, 2);
+
+        return annotated;
+    }
+}
